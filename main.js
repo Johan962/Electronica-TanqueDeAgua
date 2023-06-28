2510626b05a6b88d76c02be7e1bb5b03baba49b2
Daniel Alegria Sallo <ccusdanielalegriasallo@gmail.com>
2023-06-28 18:47:25 -0500

    fixed a bit of the logic

--- main.js
+++ main.js
@@ -1,44 +1,4 @@
-function actualizar_agua ( num )
-{
-    var agua = $(".agua");
-    diff = Number(agua.height()) + num;
-    if ( diff < 70 ){
-        diff = 70
-    } else if( diff > 400 ){
-        diff = 400
-    }
-    agua.animate( {height:`${diff}px`}, 300 );
-    actualizar_estado();
-}
-
-function actualizar_sensores ( s1, s2, s3 )
-{
-}
-
-$(document).ready(function(){
-    $("#llenar").click(
-        ( ) => {
-            actualizar_agua(30);
-            s1 = Num($("#sensor1").on);
-            s2 = Num($("#sensor2").on);
-            s3 = Num($("#sensor3").on);
-            actualizar_sensores(s1, s2, s3);
-        }
-    );
-});
-$(document).ready(function(){
-    $("#vaciar").click(
-        ( ) => {
-            actualizar_agua(-30);
-            s1 = Num($("#sensor1").on);
-            s2 = Num($("#sensor2").on);
-            s3 = Num($("#sensor3").on);
-            actualizar_sensores(s1, s2, s3);
-        }
-    );
-});
-
-// parte logica
+/// parte logica
 function caso_valido ( s1, s2, s3 )
 {
     return !s1 && !s2 && !s3 +
@@ -47,11 +7,31 @@ function caso_valido ( s1, s2, s3 )
             s1 &&  s2 &&  s3;
 }
 
-function estado_tanque ( s1, s2, s3 )
+function nuevo_nivel_agua ( num )
 {
+    var agua = $(".agua");
+    nivel = Number(agua.height()) + num;
+    if ( nivel < 70 ){
+        nivel = 70
+    } else if( nivel > 400 ){
+        nivel = 400
+    }
+    return nivel
+}
+
+function actualizar ( water_change )
+{
+    nivel = nuevo_nivel_agua( water_change );
+    agua.animate( {height:`${nivel}px`}, 300 );
+
+    s1 = Num( $("#sensor1").on );
+    s2 = Num( $("#sensor2").on );
+    s3 = Num( $("#sensor3").on );
+
     let est = -1;
     if ( caso_valido(s1,s2,s3) )
         est = s1 + s2 + s3;
+
 
     switch ( est ) {
         case 0:
@@ -65,5 +45,23 @@ function estado_tanque ( s1, s2, s3 )
         default:
             return "caso invalido";
     }
+
 }
+
+/// callback functions
+$(document).ready(function(){
+    $("#llenar").click(
+        ( ) => {
+            actualizar(30);
+        }
+    );
+});
+$(document).ready(function(){
+    $("#vaciar").click(
+        ( ) => {
+            actualizar(-30);
+        }
+    );
+});
+
 
